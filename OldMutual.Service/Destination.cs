ab78aa8a79da5aa90c8875e171b0141520ac97b5
Andile <AnNgubane@clientele.co.za>
2023-10-12 09:06:53 +0200

    adding copy methods

--- OldMutual.Service/Destination.cs
+++ OldMutual.Service/Destination.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OldMutual.Service.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace OldMutual.Service
 {
-    class Destination
+    public class Destination : IDestination
     {
+        public void SetChar(char character)
+        { 
+        }
     }
 }
--- OldMutual.Service/Source.cs
+++ OldMutual.Service/Source.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OldMutual.Service.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace OldMutual.Service
 {
-    class Source
+    public class Source : ISource 
     {
+        public char GetChar() {
+            char[] source = { 'a', 'b', 'c', '\n' };
+            return 'a';
+        }
     }
 }
