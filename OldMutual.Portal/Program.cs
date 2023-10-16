3d7ecd849e77df7c79382a9635f4871b950bddd7
Andile <AnNgubane@clientele.co.za>
2023-10-16 09:07:53 +0200

    merging evelopment

--- OldMutual.Portal/Program.cs
+++ OldMutual.Portal/Program.cs
@@ -7,6 +7,21 @@ namespace OldMutual.Portal
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string strSource = "changed";
+            char[] destination = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ', 'a', 'r', 'r', 'a', 'y' };
+
+            Console.WriteLine(destination);
+
+            strSource.CopyTo(0, destination, 4, strSource.Length);
+        
+            Console.WriteLine(destination);
+
+            strSource = "A different string";
+
+            strSource.CopyTo(2, destination, 3, 9);
+
+            Console.WriteLine(destination);
         }
     }
 }
