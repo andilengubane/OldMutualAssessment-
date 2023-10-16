37001ff096ebad12c6d13e3b9ba4be8c774c9ef1
Andile <AnNgubane@clientele.co.za>
2023-10-16 09:05:38 +0200

    adding destination and source

--- OldMutual.Portal/Program.cs
+++ OldMutual.Portal/Program.cs
@@ -6,7 +6,30 @@ namespace OldMutual.Portal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             
+            
+            string strSource = "changed";
+            char[] destination = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ', 'a', 'r', 'r', 'a', 'y' };
+
+
+            IDestination.SetChar(destination);
+            // Print the char array
+            Console.WriteLine(destination);
+
+            // Embed the source string in the destination string
+            strSource.CopyTo(0, destination, 4, strSource.Length);
+
+            // Print the resulting array
+            Console.WriteLine(destination);
+
+            strSource = "A different string";
+
+            // Embed only a section of the source string in the destination
+            strSource.CopyTo(2, destination, 3, 9);
+
+            // Print the resulting array
+            Console.WriteLine(destination);
+
         }
     }
 }
