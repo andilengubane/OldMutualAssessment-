27135eae90bcb30ca2a704f472f248fd4e9a8c49
Andile <AnNgubane@clientele.co.za>
2023-11-09 13:12:09 +0200

    portal files update

--- OldMutual.Portal/Program.cs
+++ OldMutual.Portal/Program.cs
@@ -11,18 +11,18 @@ namespace OldMutual.Portal
         static void Main(string[] args)
         {
             string strSource = " change ";
-            char[] destinationValue = { 'T', 'h', 'e', ' ', 'r', 'e', 's', 'u', 'l', 't', ' ', 's', 'e', 't', ' ', 'a','r','r','a','y' };
+            char[] destinationValue = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ','a', 'r', 'r', 'a', 'y' };
 
             Console.WriteLine(_destination.SetChar(destinationValue));
 
-            strSource.CopyTo(0, destinationValue, 3, strSource.Length);
+            strSource.CopyTo(0, destinationValue, 4, strSource.Length);
 
             Console.WriteLine(destinationValue);
 
-            strSource = " Result Output";
+            strSource = "A different string";
 
-            strSource.CopyTo(0, destinationValue, 3, 14);
-           
+            strSource.CopyTo(2, destinationValue, 3, 9);
+
             Console.WriteLine(destinationValue);
             Console.ReadKey();
         }
